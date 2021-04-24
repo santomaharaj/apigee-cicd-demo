a9a22f3f767e571c6d5710751e5978b0530a5c1b
santomaharaj <60475243+santomaharaj@users.noreply.github.com>
2021-04-24 22:18:44 +1000

    Update revision.sh
    updated org

--- revision.sh
+++ revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-revision_info=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/onlineman477-eval/apis/HR-API/deployments") 
+revision_info=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/santoapigeetrail1-eval/apis/HR-API/deployments") 
 
 previous_revision_number=$(jq -r .environment[0].revision[0].name <<< "${revision_info}" ) 
 
